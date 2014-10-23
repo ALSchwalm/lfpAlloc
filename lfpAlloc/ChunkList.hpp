d25bf2210a12f354c5147a411591abb6f45e14a6
Adam Schwalm <adamschwalm@gmail.com>
2014-10-22 19:15:36 -0500

    Switch to clang-format for formatting

--- lfpAlloc/ChunkList.hpp
+++ lfpAlloc/ChunkList.hpp
@@ -5,97 +5,99 @@
 #include <atomic>
 
 #ifndef LFP_ALLOW_BLOCKING
-static_assert(ATOMIC_POINTER_LOCK_FREE==2, "Atomic pointer is not lock-free.");
+static_assert(ATOMIC_POINTER_LOCK_FREE == 2,
+              "Atomic pointer is not lock-free.");
 #endif
 
 namespace lfpAlloc {
 
-    template<std::size_t Size>
-    struct Cell {
-        uint8_t val_[Size];
-        Cell* next_ = this+1;
-    };
+template <std::size_t Size>
+struct Cell {
+    uint8_t val_[Size];
+    Cell* next_ = this + 1;
+};
 
-    template<std::size_t Size, std::size_t AllocationsPerChunk>
-    struct Chunk {
-        Chunk() noexcept {
-            auto& last = memBlock_[AllocationsPerChunk-1];
-            last.next_ = nullptr;
-        }
-        Cell<Size> memBlock_[AllocationsPerChunk];
-    };
+template <std::size_t Size, std::size_t AllocationsPerChunk>
+struct Chunk {
+    Chunk() noexcept {
+        auto& last = memBlock_[AllocationsPerChunk - 1];
+        last.next_ = nullptr;
+    }
+    Cell<Size> memBlock_[AllocationsPerChunk];
+};
 
-    template<typename T>
-    struct Node {
-        Node() : val_(), next_(nullptr){}
-        Node(const T& t) : val_(t), next_(nullptr){}
-        T val_;
-        std::atomic<Node<T>*> next_;
-    };
+template <typename T>
+struct Node {
+    Node() : val_(), next_(nullptr) {}
+    Node(const T& t) : val_(t), next_(nullptr) {}
+    T val_;
+    std::atomic<Node<T>*> next_;
+};
 
-    template<std::size_t Size, std::size_t AllocationsPerChunk>
-    class ChunkList {
-        static constexpr auto CellSize = Size-sizeof(void*);
-        using Chunk_t = Chunk<CellSize, AllocationsPerChunk>;
-        using Cell_t = Cell<CellSize>;
+template <std::size_t Size, std::size_t AllocationsPerChunk>
+class ChunkList {
+    static constexpr auto CellSize = Size - sizeof(void*);
+    using Chunk_t = Chunk<CellSize, AllocationsPerChunk>;
+    using Cell_t = Cell<CellSize>;
 
-        using ChunkNode = Node<Chunk_t>;
-        using CellNode = Node<Cell_t*>;
-    public:
-        static ChunkList& getInstance() {
-            static ChunkList c;
-            return c;
-        }
+    using ChunkNode = Node<Chunk_t>;
+    using CellNode = Node<Cell_t*>;
 
-        Cell_t* allocateChain() {
-            CellNode* recentHead = head_.load();
-            CellNode* currentNext = nullptr;
-            do {
-                // If there are no available chains, allocate a new chunk
-                if (!recentHead) {
-                    ChunkNode* currentHandle;
+public:
+    static ChunkList& getInstance() {
+        static ChunkList c;
+        return c;
+    }
 
-                    // Make a new node
-                    auto newChunk = new ChunkNode();
+    Cell_t* allocateChain() {
+        CellNode* recentHead = head_.load();
+        CellNode* currentNext = nullptr;
+        do {
+            // If there are no available chains, allocate a new chunk
+            if (!recentHead) {
+                ChunkNode* currentHandle;
 
-                    // Add the chunk to the chain
-                    do {
-                        currentHandle = handle_.load();
-                        newChunk->next_ = currentHandle;
-                    } while(!handle_.compare_exchange_weak(currentHandle, newChunk));
-                    return &newChunk->val_.memBlock_[0];
-                }
+                // Make a new node
+                auto newChunk = new ChunkNode();
 
-                currentNext = recentHead->next_;
-            } while (!head_.compare_exchange_weak(recentHead, currentNext));
-
-            auto retnValue = recentHead->val_;
-            delete recentHead;
-            return retnValue;
-        }
-
-        void deallocateChain(Cell_t* newCell) {
-            if (!newCell) {
-                return;
+                // Add the chunk to the chain
+                do {
+                    currentHandle = handle_.load();
+                    newChunk->next_ = currentHandle;
+                } while (
+                    !handle_.compare_exchange_weak(currentHandle, newChunk));
+                return &newChunk->val_.memBlock_[0];
             }
-            CellNode* currentHead = head_.load();
 
-            // Construct a new node to be added to the linked list
-            CellNode* newHead = new CellNode(newCell);
+            currentNext = recentHead->next_;
+        } while (!head_.compare_exchange_weak(recentHead, currentNext));
 
-            // Add the chain to the linked list
-            do {
-                newHead->next_.store(currentHead, std::memory_order_release);
-            } while (!head_.compare_exchange_weak(currentHead, newHead));
+        auto retnValue = recentHead->val_;
+        delete recentHead;
+        return retnValue;
+    }
+
+    void deallocateChain(Cell_t* newCell) {
+        if (!newCell) {
+            return;
         }
+        CellNode* currentHead = head_.load();
 
-    private:
-        ChunkList() : handle_(nullptr),
-                      head_(nullptr) {}
+        // Construct a new node to be added to the linked list
+        CellNode* newHead = new CellNode(newCell);
 
-        std::atomic<ChunkNode*> handle_;
-        std::atomic<CellNode*> head_;
-    };
+        // Add the chain to the linked list
+        do {
+            newHead->next_.store(currentHead, std::memory_order_release);
+        } while (!head_.compare_exchange_weak(currentHead, newHead));
+    }
+
+private:
+    ChunkList() : handle_(nullptr), head_(nullptr) {}
+
+    std::atomic<ChunkNode*> handle_;
+    std::atomic<CellNode*> head_;
+};
 }
 
 #endif
--- lfpAlloc/Pool.hpp
+++ lfpAlloc/Pool.hpp
@@ -5,44 +5,43 @@
 #include <lfpAlloc/ChunkList.hpp>
 
 namespace lfpAlloc {
-    template<std::size_t Size, std::size_t AllocationsPerChunk>
-    class Pool {
-        using ChunkList_t = ChunkList<Size, AllocationsPerChunk>;
-    public:
-        static constexpr std::size_t CellSize = Size-sizeof(void*);
-        using Cell_t = Cell<CellSize>;
+template <std::size_t Size, std::size_t AllocationsPerChunk>
+class Pool {
+    using ChunkList_t = ChunkList<Size, AllocationsPerChunk>;
 
-        Pool() : head_(nullptr){}
+public:
+    static constexpr std::size_t CellSize = Size - sizeof(void*);
+    using Cell_t = Cell<CellSize>;
 
-        ~Pool() {
-            ChunkList_t::getInstance().deallocateChain(head_);
+    Pool() : head_(nullptr) {}
+
+    ~Pool() { ChunkList_t::getInstance().deallocateChain(head_); }
+
+    void* allocate() {
+        // Head loaded from head_
+        Cell_t* currentHead = head_;
+        Cell_t* next;
+
+        // Out of cells to allocate
+        if (!currentHead) {
+            currentHead = ChunkList_t::getInstance().allocateChain();
         }
 
-        void* allocate(){
-            // Head loaded from head_
-            Cell_t* currentHead = head_;
-            Cell_t* next;
+        next = currentHead->next_;
+        head_ = next;
+        return &currentHead->val_;
+    }
 
-            // Out of cells to allocate
-            if (!currentHead) {
-                currentHead = ChunkList_t::getInstance().allocateChain();
-            }
+    void deallocate(void* p) noexcept {
+        auto newHead = reinterpret_cast<Cell_t*>(p);
+        Cell_t* currentHead = head_;
+        newHead->next_ = currentHead;
+        head_ = newHead;
+    }
 
-            next = currentHead->next_;
-            head_ = next;
-            return &currentHead->val_;
-        }
-
-        void deallocate(void* p) noexcept {
-            auto newHead = reinterpret_cast<Cell_t*>(p);
-            Cell_t* currentHead = head_;
-            newHead->next_ = currentHead;
-            head_ = newHead;
-        }
-
-    private:
-        Cell_t* head_;
-    };
+private:
+    Cell_t* head_;
+};
 }
 
 #endif
